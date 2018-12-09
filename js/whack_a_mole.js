5abf92f6689d0515dedd49e6b9df34fcfbab2c06
WangLei <wanglei0717@outlook.com>
2018-12-09 12:04:50 +0800

    Add click event and animation for start button

---
+++ js/whack_a_mole.js
@@ -0,0 +1,39 @@
+window.onload = function () {
+  const holes = document.querySelectorAll('.hole');
+  const moles = document.querySelectorAll('.mole');
+  const startBtn = document.getElementById('start_btn');
+
+  const holeNumber = holes.length;
+  const gameTime = 10000;
+  let timeHandle = false;
+  let popHandle = false;
+  let score = 0;
+  let lastMolesStatus = new Array(holeNumber);
+  let currentMolesStatus = new Array(holeNumber);
+
+  startBtn.addEventListener('click', function() {
+    showBtnAnimation();
+    resetGame();
+    startGame();
+  }, false);
+
+  function showBtnAnimation() {
+    event.preventDefault();
+    startBtn.classList.add('animate');
+    setTimeout(() => {
+      startBtn.classList.remove('animate');
+      startBtn.classList.add('hide');
+    }, 700);
+  }
+
+  function startGame() {
+    // body...
+  }
+
+  function resetGame() {
+    // body...
+  }
+
+
+
+}
